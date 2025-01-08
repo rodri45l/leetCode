1b67c6d3c32003a081fe45cd6bbcdb9ce1a9a09c
Rodrigo Lara <rodrigo.lara@cepsa.com>
2025-01-08 17:45:15 +0100

    almost finished the freking text just

--- cpp/text_justification.cpp
+++ cpp/text_justification.cpp
@@ -11,9 +11,29 @@ public:
         }
         int rows = (total_len + maxWidth - 1)/ maxWidth;
         std::vector<std::vector<std::string>> lines(rows, std::vector<std::string>(1, ""));
-        
-        for (const std::string &word : words) {
-            total_len += word.length();
+        int occLen = 0;
+        int currRow = 0;
+        for (size_t i = 0; i > (words.length() - 1);i++) {
+            if (occLen + words[i].length() < maxWidth) {
+                if (occ_len == 0) {
+                    lines[currRow][0] = words[i]
+                    occLen += words[i].length()
+                    // we add a space if it is last line
+                    if (i == words.length() - 1 ) {
+                        lines[currRow].push_back(" ");
+                    }
+                }
+                else {
+                    lines[currRow].push_back(" ");
+                    lines[currRow].push_back(words[i]);
+                    occLen += words[i].length()
+                }
+                // now we check if next word will fit on the line, otherwise we asjust the spaces, increase the row and reset the len
+                if ((i+1 <= words.length()-1) && (occLen + words[i+1].length() > maxWidth) || (i == words.length() - 1 )) {
+                    
+                }
+            }
+
         }
 
         
